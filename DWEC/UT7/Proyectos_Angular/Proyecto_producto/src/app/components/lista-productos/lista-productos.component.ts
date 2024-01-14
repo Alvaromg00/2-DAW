d0b3bf685a33421f6acb5d77edb0878fc47d5037
Alvaromg00 <amorong03@informatica.iesvalledeljerteplasencia.es>
2024-01-14 01:48:23 +0100

    .

--- DWEC/UT7/Proyectos_Angular/Proyecto_producto/src/app/components/lista-productos/lista-productos.component.ts
+++ DWEC/UT7/Proyectos_Angular/Proyecto_producto/src/app/components/lista-productos/lista-productos.component.ts
@@ -6,10 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './lista-productos.component.scss'
 })
 export class ListaProductosComponent {
-titulo= "Lista producto";
+titulo= "Lista de productos";
 cabeceras={
-  descripcion: "Productos",
+  imagen: "Imagen",
+  descripcion: "Producto",
   precio: "Precio",
   disponibilidad: "Disponibilidad"
-}
+};
+productos=[
+  {
+    nombre: "Lenovo Ideapad",
+    precio: "460€",
+    disponible: "Sí"
+  },
+  {
+    nombre: "Playstation 5",
+    precio: "499€",
+    disponible: "No"
+  },
+  {
+    nombre: "Xiaomi Redmi Note 13",
+    precio: "280€",
+    disponible: "Sí"
+  }
+]
 }
