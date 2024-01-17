7adc2c9a6652a9d4d5e60f9c39b85c08a298eb96
Alvaromg00 <amorong03@informatica.iesvalledeljerteplasencia.es>
2024-01-17 20:19:42 +0100

    .

--- DWEC/UT7/Proyectos_Angular/Proyecto_producto/src/app/components/lista-productos/lista-productos.component.ts
+++ DWEC/UT7/Proyectos_Angular/Proyecto_producto/src/app/components/lista-productos/lista-productos.component.ts
@@ -7,37 +7,38 @@ import { IProducto } from '../interfaces/i-producto';
   styleUrl: './lista-productos.component.scss'
 })
 export class ListaProductosComponent {
-titulo= "Lista de productos";
-cabeceras={
-  imagen: "Imagen",
-  descripcion: "Producto",
-  precio: "Precio",
-  disponibilidad: "Disponibilidad"
-};
-productos: IProducto[]=[
-  {
-    id: 1,
-    descripcion: "Lenovo Ideapad",
-    disponibilidad: new Date('2020-12-02'),
-    precio: 460,
-    imagenUrl: 'assets/lenovo.jpg',
-    puntuacion: 4
-  },
-  {
-    id: 2,
-    descripcion: "Playstation 5",
-    disponibilidad: new Date('2023-10-09'),
-    precio: 499,
-    imagenUrl: 'assets/ps5.jpg',
-    puntuacion: 5
-  },
-  {
-    id: 2,
-    descripcion: "Xiaomi Redmi Note 13",
-    disponibilidad: new Date('2021-02-09'),
-    precio: 280,
-    imagenUrl: 'assets/xiaomi.jpg',
-    puntuacion: 4.2
-  }
-]
+  titulo= "Lista de productos";
+  cabeceras={
+    id: "#",
+    imagen: "Imagen",
+    descripcion: "Producto",
+    precio: "Precio",
+    disponibilidad: "Disponibilidad"
+  };
+  productos: IProducto[]=[
+    {
+      id: 1,
+      descripcion: "Lenovo Ideapad",
+      disponibilidad: new Date('2020-12-02'),
+      precio: 460,
+      imagenUrl: 'assets/lenovo.jpg',
+      puntuacion: 4
+    },
+    {
+      id: 2,
+      descripcion: "Playstation 5",
+      disponibilidad: new Date('2023-10-09'),
+      precio: 499,
+      imagenUrl: 'assets/ps5.jpg',
+      puntuacion: 5
+    },
+    {
+      id: 2,
+      descripcion: "Xiaomi Redmi Note 13",
+      disponibilidad: new Date('2021-02-09'),
+      precio: 280,
+      imagenUrl: 'assets/xiaomi.jpg',
+      puntuacion: 4.2
+    },
+  ];
 }
