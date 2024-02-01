7bcdc86660025b75aa89e7762c28ac6d72f52bee
Alvaromg00 <amorong03@informatica.iesvalledeljerteplasencia.es>
2024-02-01 14:56:52 +0100

    .

--- DWEC/UT7/Proyectos_Angular/Proyecto_producto/src/app/components/item-producto/item-producto.component.ts
+++ DWEC/UT7/Proyectos_Angular/Proyecto_producto/src/app/components/item-producto/item-producto.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { IProducto } from '../../interfaces/i-producto';
 
 @Component({
-  selector: 'app-item-producto',
+  selector: 'item-producto',
   templateUrl: './item-producto.component.html',
   styleUrl: './item-producto.component.scss'
 })
@@ -17,6 +17,34 @@ verImagenes=true;
 
 constructor(){ }
 
+mostrarPrecio(prod: IProducto){
+  alert(prod.precio)
+}
+
 ngOnInit() {   
 }
+
+mostrarImagenes: boolean = true;
+  claseBoton: string = 'btn-primary';
+
+  oculto=false;
+  textoBoton='Ocultar';
+  displayFotos='block';
+  ClasesBoton={
+    'btn':true,
+    'btn-primary':false,
+    'btn-danger':true,
+  };
+
+  mostrarOcultarImagen(){
+    this.textoBoton=!this.oculto ? 'Mostrar' : 'Ocultar'
+    this.ClasesBoton["btn-danger"]=this.oculto
+    this.ClasesBoton["btn-primary"]=!this.oculto
+    this.displayFotos=this.oculto ? 'block' : 'none'
+    this.oculto=!this.oculto;
+  }
+
+  filtroBusqueda: string = '';
+  anchoImagenes=100;
+
 }
