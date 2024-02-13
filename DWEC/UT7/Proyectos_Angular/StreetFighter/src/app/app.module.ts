ac412bb48e806d8f490c5b210860cbe5994418b4
Alvaromg00 <amorong03@informatica.iesvalledeljerteplasencia.es>
2024-02-13 23:30:23 +0100

    .

--- DWEC/UT7/Proyectos_Angular/StreetFighter/src/app/app.module.ts
+++ DWEC/UT7/Proyectos_Angular/StreetFighter/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AreaSeleccionComponent } from './area-seleccion/area-seleccion.component';
 import { RetratoLuchadorComponent } from './retrato-luchador/retrato-luchador.component';
+import { AtributosLuchadorComponent } from './atributos-luchador/atributos-luchador.component';
 
 
 @NgModule({
-  declarations: [AppComponent, AreaSeleccionComponent, RetratoLuchadorComponent,],
+  declarations: [AppComponent, AreaSeleccionComponent, RetratoLuchadorComponent, AtributosLuchadorComponent],
   imports: [
     BrowserModule,
     FormsModule,
--- DWEC/UT7/Proyectos_Angular/StreetFighter/src/app/atributos-luchador/atributos-luchador.component.ts
+++ DWEC/UT7/Proyectos_Angular/StreetFighter/src/app/atributos-luchador/atributos-luchador.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-atributos-luchador',
-  standalone: true,
-  imports: [],
   templateUrl: './atributos-luchador.component.html',
   styleUrl: './atributos-luchador.component.scss'
 })
 export class AtributosLuchadorComponent {
-
+  @Input() nombreAtributo: string ='';
+  @Input() valorAtributo: number=0;
+  
 }
